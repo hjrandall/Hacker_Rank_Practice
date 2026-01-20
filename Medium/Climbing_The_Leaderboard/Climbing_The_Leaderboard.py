7d3494a910b245e7d1df2212379bf5c37e916f2e
Hayden Randall <hayden.randall@vivint.com>
2026-01-19 20:21:01 -0700

    added code for Climbing_The_Leaderboard

---
+++ Medium/Climbing_The_Leaderboard/Climbing_The_Leaderboard.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'climbingLeaderboard' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY ranked
+#  2. INTEGER_ARRAY player
+#
+
+def climbingLeaderboard(ranked, player):
+    # Write your code here
+    sorted_ranked_list = sorted(set(ranked),reverse=True)
+    player_rankings=[]
+    for score in player:
+        if score not in sorted_ranked_list:
+            sorted_ranked_list.append(score)
+            sorted_ranked_list = sorted(sorted_ranked_list, reverse=True)
+        player_rankings.append(sorted_ranked_list.index(score)+1)
+    return player_rankings
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ranked_count = int(input().strip())
+
+    ranked = list(map(int, input().rstrip().split()))
+
+    player_count = int(input().strip())
+
+    player = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(ranked, player)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
